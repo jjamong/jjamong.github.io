38925349342623347d31b2f8e087400e11018185
jjamong <jjajangmonkey@gmail.com>
2019-12-30 01:03:47 +0900

    SEO(검색엔진최적화) 설정
    히스토리 - 0ddc3a2

--- js/contents-nav.js
+++ js/contents-nav.js
@@ -32,9 +32,9 @@ $(document).ready(function () {
 
     // 내용이 부족할 경우 컨텐츠 height 조절
     //setTimeout(function() {
-      if ($('.sidebar-main').height() > $('.columns').height()) {
-        $('.columns').css('height', $('.sidebar-main').height() + $('.footer').height() + 130)
-      }
+    if ($('.sidebar-main').height() > $('.columns').height()) {
+      $('.columns').css('height', $('.sidebar-main').height() + $('.footer').height() + 130)
+    }
     //})
   }
 
